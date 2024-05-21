c35fd0c62b6f4d9a19f20eec74930ba408b83591
nabeeltahir <nabeeltahirdeveloper@gmail.com>
2024-05-22 04:12:17 +0500

    pdf fix

--- src/app/dashboard/invoices/pdfDownload.tsx
+++ src/app/dashboard/invoices/pdfDownload.tsx
@@ -19,7 +19,7 @@ export function PdfDownload({
 	const downloadPdf = async (obj: Record<string, any>) => {
 		let config:any = {
 		  method: 'post',
-		  url: 'http://159.65.246.131/generate-pdf',
+		  url: 'https://psestimating.live/generate-pdf',
 		  headers: { 
 			'Content-Type': 'application/json',
 			'Accept': 'application/pdf'
